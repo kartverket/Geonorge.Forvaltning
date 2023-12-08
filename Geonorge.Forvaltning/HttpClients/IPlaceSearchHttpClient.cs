3b675666b339b9a64daf9f09bb58a7d5840722d3
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2023-12-08 15:46:52 +0100

    HTTP-klient for stedsøk, m.m.

---
+++ Geonorge.Forvaltning/HttpClients/IPlaceSearchHttpClient.cs
@@ -0,0 +1,9 @@
+﻿using GeoJSON.Text.Feature;
+
+namespace Geonorge.Forvaltning.HttpClients
+{
+    public interface IPlaceSearchHttpClient
+    {
+        Task<FeatureCollection> SearchAsync(string searchString, int crs);
+    }
+}
